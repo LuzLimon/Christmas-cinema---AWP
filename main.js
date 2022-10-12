55cc2033b02696508347df4f10b8193e36fa33b2
Luz Elena Limón de la Cruz <luzelenalimondelacruz123@gmail.com>
2022-10-12 17:43:16 -0500

    Feat: Added the web app.

---
+++ main.js
@@ -0,0 +1,9 @@
+//Service Worker
+if('serviceWorker' in navigator){
+    console.log('Puede usar los SW en tu navegador');
+    navigator.serviceWorker.register('./SW.js')
+                            .then(res=>console.log('serviceWorker cargando correctamente',res))
+                            .catch(err => console.log('serviceWorker no se ha podido registrar', err))
+}else{
+    console.log('NO PUEDE usar el SW en tu navegador');
+}
